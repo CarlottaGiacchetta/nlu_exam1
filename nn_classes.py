3796c345dde1ebdb6d33e2149d0e9530f8ce2382
Carlotta Giacchetta <85078090+CarlottaGiacchetta@users.noreply.github.com>
2024-04-22 15:47:06 +0200

    ajksnk

--- nn_classes.py
+++ nn_classes.py
@@ -139,40 +139,28 @@ class LM_LSTM_wt(nn.Module):
 
 ''' 
 
+    
 class LM_LSTM_wt(nn.Module):
     def __init__(self, emb_size, hidden_size, output_size, pad_index=0, out_dropout=0.1,
                  emb_dropout=0.1, n_layers=1):
         super(LM_LSTM_wt, self).__init__()
         # Token ids to vectors, we will better see this in the next lab 
         self.embedding = nn.Embedding(output_size, emb_size, padding_idx=pad_index)
-        
-        #aggiungi dropout layer 
-        self.emb_dropout = nn.Dropout(emb_dropout)
-
         # Pytorch's RNN layer: https://pytorch.org/docs/stable/generated/torch.nn.RNN.html
         self.lstm = nn.LSTM(emb_size, hidden_size, n_layers, bidirectional=False, batch_first=True)    
         self.pad_token = pad_index
-
-        #aggiungi dropout layer 
-        self.out_dropout = nn.Dropout(out_dropout)
-
         # Linear layer to project the hidden layer to our output space 
         self.output = nn.Linear(hidden_size, output_size)
-
         #weight tying
         self.output.weight = self.embedding.weight
         
     def forward(self, input_sequence):
         emb = self.embedding(input_sequence)
-        #dropout
-        emb_dropout = self.emb_dropout(emb)
-        lstm_out, _  = self.lstm(emb_dropout)
-        #dropout
-        output_dropout = self.out_dropout(lstm_out)
-        output = self.output(output_dropout).permute(0,2,1)
+        lstm_out, _  = self.lstm(emb)
+        #permute(0,2,1) viene utilizzato per riordinare le dimensioni dell'output per adattarsi alla forma richiesta. 
+        #In particolare, trasforma le dimensioni da (batch_size, seq_len, output_size) a (batch_size, output_size, seq_len)
+        output = self.output(lstm_out).permute(0,2,1)
         return output
-    
-
 
 class LM_LSTM_wt_dropout(nn.Module):
     def __init__(self, emb_size, hidden_size, output_size, pad_index=0, out_dropout=0.1,
