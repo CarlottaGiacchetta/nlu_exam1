773aa2b1239e593a3d0cf1177bfd26170259a0e3
Carlotta Giacchetta <85078090+CarlottaGiacchetta@users.noreply.github.com>
2024-05-04 08:31:50 +0200

    skalk

---
+++ 2/NLU/PART1/functions.py
@@ -0,0 +1,2 @@
+#write all the other required functions (taken from the notebook and/or written on your own) 
+#needed to complete the exercise.
---
+++ 2/NLU/PART1/main.py
@@ -0,0 +1,1 @@
+#write the calls to the functions needed to output the results asked by the exercise
